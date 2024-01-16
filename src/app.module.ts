4137f9409623eb42296208c191482af1ab64e9cb
mark_kim <mark340v@gmail.com>
2024-01-16 20:35:12 +0900

    chore: 초기세팅, 환경세팅

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import appConfig from './config/app.config';
+import databaseConfig from './config/database.config';
+import { LoggerModule } from './logger/logger.module';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [appConfig, databaseConfig],
+    }),
+    LoggerModule,
+    DatabaseModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
